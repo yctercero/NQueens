31dc1144c75c1fc83a8430fc7a40a4d96e41c1aa
Yara TP <ytercero36@gmail.com>
2016-08-03 23:30:09 -0400

    completed diagonals check

--- src/Board.js
+++ src/Board.js
@@ -145,12 +145,34 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      // counter
+      var counter = 0;
+      var major = majorDiagonalColumnIndexAtFirstRow;
+
+      for (var i = 0; i < this.get("n"); i++) {
+        var cellValue = this.rows()[i][major];
+        if(cellValue === 1){
+          counter++;
+        }
+        major++;
+      }
+
+      return counter > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var nValue = this.get('n');
+
+      for (var i = (-nValue+1); i < nValue; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
+      //for loop starting with -n+1 and goes until n
+        //call first function with (i)
+
     },
 
 
@@ -160,12 +182,35 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      //create a counter
+      //create var to store arg
+      //nValue
+      var counter = 0;
+      var minor = minorDiagonalColumnIndexAtFirstRow;
+      var nValue = this.get('n');
+
+      //for loop starting at 0, go through n value
+        //check if equals one, if so increment counter
+      //increment arg  by 1
+      for (var i=0; i<nValue; i++) {
+        if (this.rows()[i][minor] === 1) {
+          counter++;
+        }
+        minor--;
+      }
+      return counter > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var nValue = this.get('n');
+
+      for (var i = nValue * 2 - 2; i >= 0; i--) {
+        if(this.hasMinorDiagonalConflictAt(i)){
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
